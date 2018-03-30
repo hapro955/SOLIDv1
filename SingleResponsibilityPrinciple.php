00e8ed513de8b390cfc7c63070eba9ea5e7411d7
hapro955 <hapro955@gmail.com>
2018-03-30 13:15:07 +0700

    add function setName

--- SingleResponsibilityPrinciple.php
+++ SingleResponsibilityPrinciple.php
@@ -5,8 +5,12 @@
 		
 	}
 
-	public function __construct($name = "", $age = ""){
+	public function __construct($name = "", $age = "") {
 		$this->name = $name;
 		$this->age = $age;
+	}
+
+	public function setName($name) {
+		$this->name = $name;
 	}
 ?>
